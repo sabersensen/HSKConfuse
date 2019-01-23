b86b7aaa74f3f79f6c6350b7331dd0fb30db791c
zoushixin <296902026@qq.com>
2019-01-23 15:01:59 +0800

    上传混淆

--- HSKConfuse/Resource/codeObfuscation.h
+++ HSKConfuse/Resource/codeObfuscation.h
@@ -1,7 +1,9 @@
 #ifndef Demo_codeObfuscation_h
 #define Demo_codeObfuscation_h
-//confuse string at Mon May 14 17:05:20 CST 2018
-#define hsk_Function1 NSZoSDlefQBUZrZC
-#define hsk_Function2 xfkOsqfrvoEGHwUe
-#define hsk_start RKPIGyFHgwAQGtlC
+//confuse string at Tue May 15 10:31:32 CST 2018
+#define fc_abbcc FKIeSNHtHGgZAqSL
+#define fc_abc xPaighTbVZNZMxoE
+#define fc_bcd YojmbzZIUoObUeIj
+#define fc_dfg qXDNiwVALVnnRWsu
+#define fc_model wMKxlYfiTqrSbSwL
 #endif
